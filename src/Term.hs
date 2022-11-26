87e7290bfe070871f92e4a91d7c2c7a13ad739f4
Dylan Göpel <dylan.connor@t-online.de>
2022-11-26 12:57:11 +0100

    add prototype implementation for elimExpr

--- src/Term.hs
+++ src/Term.hs
@@ -7,13 +7,16 @@ import Parser
 data Term = Appl Term Term |
             Fn Term Term |
             Arg Int |
+            Ref String Term |
             Sym String |
             IndT Term |
             Constr Term |
             Elim Term |
-            UnitT |
-            UnitElem |
-            BoolT |
+            Unit |
+            Point |
+            Bool |
+            TrueT |
+            FalseT |
             If
     deriving (Show, Eq)
 
@@ -26,6 +29,7 @@ trec r (Sym s) = Sym s
 trec r (IndT t) = IndT (r t)
 trec r (Constr t) = Constr (r t)
 trec r (Elim t) = Elim (r t)
+trec r (Ref s t) = Ref s $ Elim $ r t
 trec _ x = x
 
 -- recursively substitute (Sym v) with the appropriate debrujin index 
@@ -33,6 +37,12 @@ subdeb :: String -> Int -> Term -> Term
 subdeb s n (Sym v) = if v == s then Arg n else Sym v
 subdeb s n (Fn t x) = Fn (subdeb s n t) (subdeb s (n + 1) x)
 subdeb s n x = trec (subdeb s n) x
+
+subt :: String -> Term -> Term -> Term
+subt pat t (Sym s) = if pat == s then t else Sym s
+subt pat t x = trec (subt pat t) x
+
+subref s x = subt s (Ref s x)
 
 cases :: [a -> Maybe b] -> a -> Maybe b
 cases [] _ = Nothing
@@ -53,5 +63,14 @@ unsym (Sym s) = Just s
 unsym _ = Nothing
 
 elimExpr :: Expr -> Maybe Term
-elimExpr = elimForms [("fn :v :t :y", \[v, t, y] -> unsym v >>= \vv -> return $ Fn t (subdeb vv 0 y)),
-                      (":f :x", \[f, x] -> return $ Appl f x)]
+elimExpr (Symb s) = case s of
+    "unit" -> return Unit
+    "pt" -> return Point
+    "bool" -> return Bool
+    "true" -> return TrueT
+    "false" -> return FalseT
+    "if" -> return If
+    s -> return $ Sym s
+elimExpr e = elimForms [("fn :v :t :y", \[v, t, y] -> unsym v >>= \vv -> return $ Fn t (subdeb vv 0 y)),
+                        ("let :s = :x in :y", \[s, x, y] -> unsym s >>= \ss -> return $ subref ss x y),
+                        (":f :x", \[f, x] -> return $ Appl f x)] e
